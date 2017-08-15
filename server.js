d4709ab234a24b5a8d0bb41920c4fa789c70045d
kvborkar100 <30365773+kvborkar100@users.noreply.github.com>
2017-08-15 16:06:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,15 +9,15 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article 1',function(req,res){
+app.get('/article-1',function(req,res){
   res.send('article one is requested');  
 });
 
-app.get('/article 2',function(req,res){
+app.get('/article-2',function(req,res){
   res.send('article two is requested');  
 });
 
-app.get('/article 3',function(req,res){
+app.get('/article-3',function(req,res){
   res.send('article three is requested');  
 });
 
