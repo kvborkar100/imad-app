f46b60f49ef0a9db39b3436175e34405975a541c
kvborkar100 <30365773+kvborkar100@users.noreply.github.com>
2017-08-17 17:44:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML='HI HI HI';
 
-var img = documentgetElementById('img');
+var img = documentgetElementById('madi');
 
 img.onclick=function (){
   img.style.marginleft='100px';  
